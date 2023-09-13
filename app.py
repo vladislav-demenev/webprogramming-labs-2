709306b0dc5b73fbc72b76533f20c1852f9278a4
Владислав Деменев <v.demenev1905@gmail.com>
2023-09-13 17:12:57 +0700

    Меню

--- app.py
+++ app.py
@@ -2,9 +2,53 @@ from flask import Flask
 app=Flask(__name__)
 
 @app.route("/")
+def slesh():
+    return """
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>НГТУ, ФБ, Лабораторные работы</title>
+    </head>
+    <body>
+        <header>
+            НГТУ, ФБ, WEB-программирование, часть 2. Список лабораторных
+        </header>
+
+        <h2><a href="/lab1" target="_blank">Первая лабораторная</a></h2>
+
+        <footer>
+            &copy; Владислав Деменев, ФБИ-13, 3 курс, 2023
+        </footer>
+    </body>
+</html>
+"""
+
+@app.route("/index")
 def start():
     return """
 <!DOCTYPE html>
+<html>
+    <head>
+        <title>НГТУ, ФБ, Лабораторные работы</title>
+    </head>
+    <body>
+        <header>
+            НГТУ, ФБ, WEB-программирование, часть 2. Список лабораторных
+        </header>
+
+        <h2><a href="/lab1" target="_blank">Первая лабораторная</a></h2>
+
+        <footer>
+            &copy; Владислав Деменев, ФБИ-13, 3 курс, 2023
+        </footer>
+    </body>
+</html>
+"""
+
+@app.route('/lab1')
+def lab1():
+    return """
+<!doctype html>
 <html>
     <head>
         <title>Деменев Владислав Вячеславович, Лабораторная 1</title>
@@ -14,7 +58,13 @@ def start():
             НГТУ, ФБ, Лабораторная работа 1
         </header>
 
-        <h1>web-сервер на flask</h1>
+        <h1>
+            Flask — фреймворк для создания веб-приложений на языке
+            программирования Python, использующий набор инструментов
+            Werkzeug, а также шаблонизатор Jinja2. Относится к категории так
+            называемых микрофреймворков — минималистичных каркасов
+            веб-приложений, сознательно предоставляющих лишь самые базовые возможности.         
+        </h1>
 
         <footer>
             &copy; Владислав Деменев, ФБИ-13, 3 курс, 2023
