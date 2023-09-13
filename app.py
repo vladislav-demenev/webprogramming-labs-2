bacd6e4b3093af3d7852cac3ddbfa90990b9d5e8
Владислав Деменев <v.demenev1905@gmail.com>
2023-09-13 16:48:04 +0700

    Вывод страницы первой лабораторной

--- app.py
+++ app.py
@@ -3,4 +3,22 @@ app=Flask(__name__)
 
 @app.route("/")
 def start():
-    return "web-сервер на flask"
+    return """
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Деменев Владислав Вячеславович, Лабораторная 1</title>
+    </head>
+    <body>
+        <header>
+            НГТУ, ФБ, Лабораторная работа 1
+        </header>
+
+        <h1>web-сервер на flask</h1>
+
+        <footer>
+            &copy; Владислав Деменев, ФБИ-13, 3 курс, 2023
+        </footer>
+    </body>
+</html>
+"""
