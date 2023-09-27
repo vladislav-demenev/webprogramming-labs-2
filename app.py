a3c4fc22b8313cd9751f849844667982ccbf6e6c
Vladislav Demenev <v.demenev1905@gmail.com>
2023-09-27 17:03:11 +0700

    Книги

--- app.py
+++ app.py
@@ -207,4 +207,17 @@ def example():
         {'name': 'мандарины', 'price': 95}, 
         {'name': 'манго', 'price': 321},
     ]
-    return render_template('example.html', name=name, number=number, groupe=groupe, course=course, fruits=fruits)
+
+    books = [
+        {'author': 'Маргарет Митчелл', 'name': 'Унесенные ветром', 'genre': 'роман', 'pages': '1056'},
+        {'author': 'Джордж Оруэлл', 'name': '1984', 'genre': 'роман', 'pages': '1152'},
+        {'author': 'Михаил Булгаков', 'name': 'Мастер и Маргарита', 'genre': 'роман-эпопея', 'pages': '1360'},
+        {'author': 'Лев Толстой', 'name': 'Война и мир', 'genre': 'роман', 'pages': '1376'},
+        {'author': 'Харпер Ли', 'name': 'Убить пересмешника', 'genre': 'роман', 'pages': '1424'},
+        {'author': 'Эмили Бронте', 'name': 'Грозовой перевал', 'genre': 'роман', 'pages': '1472'},
+        {'author': 'Рэй Брэдбери', 'name': '451° по Фаренгейту', 'genre': 'роман', 'pages': '1492'},
+        {'author': 'Владимир Набоков', 'name': 'Лолита', 'genre': 'роман', 'pages': '1774'},
+        {'author': 'Артур Конан Дойл', 'name': 'Весь Шерлок Холмс', 'genre': 'роман', 'pages': '3031'},
+        {'author': 'Габриэль Гарсиа Маркес', 'name': 'Недобрый час', 'genre': 'биография', 'pages': '3600'},
+    ]
+    return render_template('example.html', name=name, number=number, groupe=groupe, course=course, fruits=fruits, books=books)
