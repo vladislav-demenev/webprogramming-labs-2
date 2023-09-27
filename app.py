95890c4a3a9c4459f22fe430a994e10c31debdbc
Vladislav Demenev <v.demenev1905@gmail.com>
2023-09-27 16:30:43 +0700

    Заготовка шаблона

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, render_template 
 app=Flask(__name__)
 
 @app.route("/")
@@ -196,3 +196,7 @@ def lamba():
     </body>
 </html>
 '''
+
+@app.route('/lab2/example')
+def example():
+    return render_template('example.html')
