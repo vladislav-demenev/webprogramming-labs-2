8b58f9273eb376df1278d626a8b531460c613da2
Студент Факультета Бизнеса <student@FB.LOCAL>
2023-10-11 17:18:44 +0700

    Форма

--- lab3.py
+++ lab3.py
@@ -1,7 +1,15 @@
-from flask import Blueprint, redirect, url_for, render_template
+from flask import Blueprint, redirect, url_for, render_template, request
 lab3 = Blueprint('lab3',__name__)
 
 
 @lab3.route('/lab3/')
 def lab():
     return render_template('lab3.html')
+
+
+@lab3.route('/lab3/form1')
+def form1():
+    user = request.args.get('user')
+    age = request.args.get('age')
+    sex = request.args.get('sex')
+    return render_template('form1.html', user=user, age=age, sex=sex)
