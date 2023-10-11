2997e78f40e81ff44323932c9d654d6eeadf4b9e
Студент Факультета Бизнеса <student@FB.LOCAL>
2023-10-11 17:42:52 +0700

    Заказ напитка

--- lab3.py
+++ lab3.py
@@ -18,3 +18,33 @@ def form1():
         errors['age'] = 'Введите возвраст!'
     sex = request.args.get('sex')
     return render_template('form1.html', user=user, age=age, sex=sex, errors=errors)
+
+
+@lab3.route('/lab3/order')
+def order():
+    return render_template('order.html')
+
+
+@lab3.route('/lab3/pay')
+def pay():
+    price = 0
+
+    drink = request.args.get('drink')
+    if drink == 'coffee':
+        price = 120
+    elif drink == 'black-tea':
+        price = 80
+    else:
+        price = 70
+
+    if request.args.get('milk') == 'on':
+        price += 30
+    if request.args.get('sugar') == 'on':
+        price += 10    
+
+    return render_template('pay.html', price=price)
+
+
+@lab3.route('/lab3/success')
+def success():
+    return render_template('success.html')
