a27b26c31333cd3953954016eedcaf161d8629a8
Студент Факультета Бизнеса <student@FB.LOCAL>
2023-12-06 17:55:26 +0700

    Конец 7 лабы

--- lab7.py
+++ lab7.py
@@ -1,15 +1,11 @@
-from werkzeug.security import check_password_hash, generate_password_hash
-from flask import Blueprint, render_template, request, make_response, redirect, session
-import psycopg2
-from Db import db
-from Db.models import users, articles
-from flask_login import login_user, login_required, current_user, logout_user
+from flask import Blueprint,render_template, request, redirect, session, abort
 
 lab7 = Blueprint('lab7', __name__)
 
 @lab7.route('/lab7/')
 def main():
     return render_template('lab7/index.html')
+
 
 @lab7.route('/lab7/drink')
 def drink():
@@ -18,16 +14,17 @@ def drink():
 @lab7.route('/lab7/api', methods = ['POST'])
 def api():
     data = request.json
-    
+
     if data['method'] == 'get-price':
         return get_price(data['params'])
+    
     if data['method'] == 'pay':
         return pay(data['params'])
     
     abort(400)
 
 def get_price(params):
-    return {"result": calculate_price(params), "error": None}
+    return {"result": calculate_price(params), "errors": None}
 
 def calculate_price(params):
     drink = params['drink']
@@ -40,23 +37,48 @@ def calculate_price(params):
         price = 80
     else:
         price = 70
-    
+
     if milk:
         price += 30
     if sugar:
         price += 10
 
     return price
-    
+
 def pay(params):
     card_num = params['card_num']
     if len(card_num) != 16 or not card_num.isdigit():
-        return {"result": None, "error": "Неверный номер карты"}
+        return{"result":None, "error": "Неверный номер карты"}
     
     cvv = params['cvv']
-    if len(cvv) != 3 or not card_num.isdigit():
-        return {"result": None, "error": "Неверный номер CVV/CVC"}
+    if len(cvv) != 3 or not cvv.isdigit():
+        return{"result":None, "error": "Неверный номер CVV/CVC"}
     
     price = calculate_price(params)
-    return {"result": f'С карты {card_num} списано {price} руб.', "error": None}
-    
+    return{"result": f'С карты {card_num} списано {price} руб', "error": None}
+
+
+@lab7.route('/lab7/refund', methods=['POST'])
+def refund():
+    data = request.json
+
+    card_num = data['params']['card_num']
+    cvv = data['params']['cvv']
+    drink = data['params']['drink']
+    milk = data['params']['milk']
+    sugar = data['params']['sugar']
+
+    if not is_valid_card(card_num):
+        return {"result": None, "error": "Неверный номер карты"}
+
+    if not is_valid_cvv(cvv):
+        return {"result": None, "error": "Неверный номер CVV/CVC"}
+
+    refund_amount = calculate_price(data['params'])
+    return {"result": f'Деньги вернулись на карту: {refund_amount} руб', "error": None}
+
+def is_valid_card(card_num):
+    return len(card_num) == 16 and card_num.isdigit()
+
+def is_valid_cvv(cvv):
+    return len(cvv) == 3 and cvv.isdigit()
--- static/lab7/drink.js
+++ static/lab7/drink.js
@@ -1,4 +1,4 @@
-function getPrice() {
+function get_price() {
     const milk = document.querySelector('[name=milk]').checked;
     const sugar = document.querySelector('[name=sugar]').checked;
     const drink = document.querySelector('[name=drink]:checked').value;
@@ -10,18 +10,92 @@ function getPrice() {
             milk: milk,
             sugar: sugar
         }
-    }
+    };
 
     fetch('/lab7/api', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(obj)
+        })
+        .then(function(resp) {
+            return resp.json();
+        })
+        .then(function(data){
+            document.querySelector('#price').innerHTML = `Цена напитка: ${data.result} руб`;
+            document.querySelector('#pay').style.display = '';
+            document.querySelector('#refund').style.display = '';
+        })
+}
+
+function pay() {
+    const cardNum = document.querySelector('[name=card]').value;
+    const cardName = document.querySelector('[name=name]').value;
+    const cvv = document.querySelector('[name=cvv]').value;
+    const milk = document.querySelector('[name=milk]').checked;
+    const sugar = document.querySelector('[name=sugar]').checked;
+    const drink = document.querySelector('[name=drink]:checked').value;
+
+    const obj = {
+        "method": "pay",
+        "params": {
+            card_num: cardNum,
+            card_name: cardName,
+            cvv: cvv,
+            drink: drink,
+            milk: milk,
+            sugar: sugar
+        }
+    };
+
+    fetch('/lab7/api', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(obj)
+        })
+        .then(function(resp) {
+            return resp.json();
+        })
+        .then(function(data){
+            if (data.error) {
+                alert(data.error);
+            } else {
+                alert(data.result);
+            }
+        })
+}
+
+
+function refund() {
+    const cardNum = document.querySelector('[name=card]').value;
+    const cvv = document.querySelector('[name=cvv]').value;
+    const drink = document.querySelector('[name=drink]:checked').value;
+    const milk = document.querySelector('[name=milk]').checked;
+    const sugar = document.querySelector('[name=sugar]').checked;
+
+    const obj = {
+        "method": "refund",
+        "params": {
+            card_num: cardNum,
+            cvv: cvv,
+            drink: drink,
+            milk: milk,
+            sugar: sugar
+        }
+    };
+
+    fetch('/lab7/refund', {
         method: 'POST',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify(obj)
     })
-    .then(function(data) {
+    .then(function(resp) {
         return resp.json();
     })
-    .then(function(data) {
-        document.querySelector('#price').innerHTML = `Цена напитка: ${data.result} руб.`;
+    .then(function(data){
+        if (data.error) {
+            alert(data.error);
+        } else {
+            alert(data.result);
+        }
     })
-};
-
+}
