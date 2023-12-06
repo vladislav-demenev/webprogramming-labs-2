5a82146b8b9c89ebd24739cf7ac273c05946273b
Студент Факультета Бизнеса <student@FB.LOCAL>
2023-12-06 18:54:21 +0700

    Конец 7 лабы

--- lab7.py
+++ lab7.py
@@ -1,84 +1,106 @@
-from flask import Blueprint,render_template, request, redirect, session, abort
+from flask import Blueprint, render_template, request, redirect, session, abort
 
+# Создание Flask Blueprint с именем 'lab7'
 lab7 = Blueprint('lab7', __name__)
 
+# Роут для отображения главной страницы
 @lab7.route('/lab7/')
 def main():
-    return render_template('lab7/index.html')
+   return render_template('lab7/index.html')
 
-
+# Роут для отображения страницы выбора напитка
 @lab7.route('/lab7/drink')
 def drink():
-    return render_template('lab7/drink.html')
+   return render_template('lab7/drink.html')
 
-@lab7.route('/lab7/api', methods = ['POST'])
+# Роут API для обработки POST-запросов
+@lab7.route('/lab7/api', methods=['POST'])
 def api():
-    data = request.json
+   data = request.json
 
-    if data['method'] == 'get-price':
-        return get_price(data['params'])
-    
-    if data['method'] == 'pay':
-        return pay(data['params'])
-    
-    abort(400)
+   # Обработка запроса 'get-price'
+   if data['method'] == 'get-price':
+       return get_price(data['params'])
 
+   # Обработка запроса 'pay'
+   if data['method'] == 'pay':
+       return pay(data['params'])
+
+   # Если метод не определен или неверен, вернуть ошибку 400
+   abort(400)
+
+# Функция для получения цены напитка
 def get_price(params):
-    return {"result": calculate_price(params), "errors": None}
+   return {"result": calculate_price(params), "errors": None}
 
+# Функция для расчета цены напитка на основе параметров
 def calculate_price(params):
-    drink = params['drink']
-    milk = params['milk']
-    sugar = params['sugar']
+   drink = params['drink']
+   milk = params['milk']
+   sugar = params['sugar']
 
-    if drink == 'coffee':
-        price = 120
-    elif drink == 'black-tea':
-        price = 80
-    else:
-        price = 70
+   # Определение цены в зависимости от выбранного напитка
+   if drink == 'coffee':
+       price = 120
+   elif drink == 'black-tea':
+       price = 80
+   else:
+       price = 70
 
-    if milk:
-        price += 30
-    if sugar:
-        price += 10
+   # Добавление цены за молоко и сахар, если выбраны
+   if milk:
+       price += 30
+   if sugar:
+       price += 10
 
-    return price
+   return price
 
+# Функция для обработки платежа
 def pay(params):
-    card_num = params['card_num']
-    if len(card_num) != 16 or not card_num.isdigit():
-        return{"result":None, "error": "Неверный номер карты"}
-    
-    cvv = params['cvv']
-    if len(cvv) != 3 or not cvv.isdigit():
-        return{"result":None, "error": "Неверный номер CVV/CVC"}
-    
-    price = calculate_price(params)
-    return{"result": f'С карты {card_num} списано {price} руб', "error": None}
+   card_num = params['card_num']
 
+   # Проверка формата номера карты
+   if len(card_num) != 16 or not card_num.isdigit():
+       return {"result": None, "error": "Неверный номер карты"}
 
+   cvv = params['cvv']
+
+   # Проверка формата CVV
+   if len(cvv) != 3 or not cvv.isdigit():
+       return {"result": None, "error": "Неверный номер CVV/CVC"}
+
+   # Расчет цены и возврат результата платежа
+   price = calculate_price(params)
+   return {"result": f'С карты {card_num} списано {price} руб', "error": None}
+
+# Роут для обработки запросов на возврат
 @lab7.route('/lab7/refund', methods=['POST'])
 def refund():
-    data = request.json
+   data = request.json
 
-    card_num = data['params']['card_num']
-    cvv = data['params']['cvv']
-    drink = data['params']['drink']
-    milk = data['params']['milk']
-    sugar = data['params']['sugar']
+   # Извлечение данных о карте, напитке, молоке и сахаре из запроса
+   card_num = data['params']['card_num']
+   cvv = data['params']['cvv']
+   drink = data['params']['drink']
+   milk = data['params']['milk']
+   sugar = data['params']['sugar']
 
-    if not is_valid_card(card_num):
-        return {"result": None, "error": "Неверный номер карты"}
+   # Проверка валидности номера карты
+   if not is_valid_card(card_num):
+       return {"result": None, "error": "Неверный номер карты"}
 
-    if not is_valid_cvv(cvv):
-        return {"result": None, "error": "Неверный номер CVV/CVC"}
+   # Проверка валидности CVV
+   if not is_valid_cvv(cvv):
+       return {"result": None, "error": "Неверный номер CVV/CVC"}
 
-    refund_amount = calculate_price(data['params'])
-    return {"result": f'Деньги вернулись на карту: {refund_amount} руб', "error": None}
+   # Расчет суммы возврата и возврат результата
+   refund_amount = calculate_price(data['params'])
+   return {"result": f'Деньги вернулись на карту: {refund_amount} руб', "error": None}
 
+# Функция для проверки валидности номера карты
 def is_valid_card(card_num):
-    return len(card_num) == 16 and card_num.isdigit()
+   return len(card_num) == 16 and card_num.isdigit()
 
+# Функция для проверки валидности CVV
 def is_valid_cvv(cvv):
-    return len(cvv) == 3 and cvv.isdigit()
+   return len(cvv) == 3 and cvv.isdigit()
--- static/lab7/drink.js
+++ static/lab7/drink.js
@@ -1,8 +1,10 @@
+// Функция для получения цены напитка
 function get_price() {
     const milk = document.querySelector('[name=milk]').checked;
     const sugar = document.querySelector('[name=sugar]').checked;
     const drink = document.querySelector('[name=drink]:checked').value;
-
+ 
+    // Создание объекта для отправки POST-запроса
     const obj = {
         "method": "get-price",
         "params": {
@@ -11,30 +13,34 @@ function get_price() {
             sugar: sugar
         }
     };
-
+ 
+    // Выполнение POST-запроса с использованием Fetch API
     fetch('/lab7/api', {
-            method: 'POST',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify(obj)
-        })
-        .then(function(resp) {
-            return resp.json();
-        })
-        .then(function(data){
-            document.querySelector('#price').innerHTML = `Цена напитка: ${data.result} руб`;
-            document.querySelector('#pay').style.display = '';
-            document.querySelector('#refund').style.display = '';
-        })
-}
-
-function pay() {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(obj)
+    })
+    .then(function(resp) {
+        return resp.json();
+    })
+    .then(function(data){
+        // Обновление интерфейса с полученной ценой
+        document.querySelector('#price').innerHTML = `Цена напитка: ${data.result} руб`;
+        document.querySelector('#pay').style.display = '';
+        document.querySelector('#refund').style.display = '';
+    })
+ }
+ 
+ // Функция для обработки платежа
+ function pay() {
     const cardNum = document.querySelector('[name=card]').value;
     const cardName = document.querySelector('[name=name]').value;
     const cvv = document.querySelector('[name=cvv]').value;
     const milk = document.querySelector('[name=milk]').checked;
     const sugar = document.querySelector('[name=sugar]').checked;
     const drink = document.querySelector('[name=drink]:checked').value;
-
+ 
+    // Создание объекта для отправки POST-запроса
     const obj = {
         "method": "pay",
         "params": {
@@ -46,32 +52,35 @@ function pay() {
             sugar: sugar
         }
     };
-
+ 
+    // Выполнение POST-запроса с использованием Fetch API
     fetch('/lab7/api', {
-            method: 'POST',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify(obj)
-        })
-        .then(function(resp) {
-            return resp.json();
-        })
-        .then(function(data){
-            if (data.error) {
-                alert(data.error);
-            } else {
-                alert(data.result);
-            }
-        })
-}
-
-
-function refund() {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(obj)
+    })
+    .then(function(resp) {
+        return resp.json();
+    })
+    .then(function(data){
+        // Вывод сообщения об успешном платеже или ошибке
+        if (data.error) {
+            alert(data.error);
+        } else {
+            alert(data.result);
+        }
+    })
+ }
+ 
+ // Функция для запроса на возврат
+ function refund() {
     const cardNum = document.querySelector('[name=card]').value;
     const cvv = document.querySelector('[name=cvv]').value;
     const drink = document.querySelector('[name=drink]:checked').value;
     const milk = document.querySelector('[name=milk]').checked;
     const sugar = document.querySelector('[name=sugar]').checked;
-
+ 
+    // Создание объекта для отправки POST-запроса
     const obj = {
         "method": "refund",
         "params": {
@@ -82,7 +91,8 @@ function refund() {
             sugar: sugar
         }
     };
-
+ 
+    // Выполнение POST-запроса с использованием Fetch API
     fetch('/lab7/refund', {
         method: 'POST',
         headers: {'Content-Type': 'application/json'},
@@ -92,10 +102,11 @@ function refund() {
         return resp.json();
     })
     .then(function(data){
+        // Вывод сообщения об успешном возврате или ошибке
         if (data.error) {
             alert(data.error);
         } else {
             alert(data.result);
         }
     })
-}
+ }
