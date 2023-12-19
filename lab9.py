e284ae1a716bfe470b22debf3ecffb88df2df7d9
Владислав Деменев <v.demenev@gmail.com>
2023-12-19 15:50:19 +0700

    Открытка

--- lab9.py
+++ lab9.py
@@ -1,5 +1,4 @@
-from flask import Blueprint, render_template, request, abort, jsonify
-from datetime import datetime
+from flask import Blueprint, render_template, request, abort, jsonify, url_for, redirect
 
 lab9 = Blueprint('lab9', __name__)
 
@@ -9,9 +8,34 @@ def main():
 
 @lab9.app_errorhandler(404)
 def not_found(e):
-    return 'Нет такой страницы, вернуться обратно <a href="/menu">Меню</a> ❄️❄️❄️', 404
+    return 'Нет такой страницы, вернуться обратно ❄️❄️❄️ <a href="/menu">Меню</a> ❄️❄️❄️', 404
 
 
 @lab9.route('/lab9/500')
 def main1():
     return render_template('lab9/500.html'), 500
+
+@lab9.route('/lab9/greeting_card', methods=['GET', 'POST'])
+def greeting_card():
+    if request.method == 'POST':
+        recipient_name = request.form.get('recipient_name')
+        recipient_gender = request.form.get('recipient_gender')
+        sender_name = request.form.get('sender_name')
+
+        # Перенаправление на URL с параметрами
+        return redirect(url_for('lab9.show_card', recipient_name=recipient_name,
+                                recipient_gender=recipient_gender, sender_name=sender_name))
+
+    return render_template('lab9/greeting_card_form.html')
+
+@lab9.route('/lab9/show_card')
+def show_card():
+    # Получение параметров из URL
+    recipient_name = request.args.get('recipient_name')
+    recipient_gender = request.args.get('recipient_gender')
+    sender_name = request.args.get('sender_name')
+
+    # Создание текста открытки
+    card_text = f"С Новым Годом, {recipient_name}! Желаю быть счастлив{'ой' if recipient_gender == 'female' else 'ым'}! С наилучшими пожеланиями, {sender_name}!"
+
+    return render_template('lab9/greeting_card_result.html', card_text=card_text)
